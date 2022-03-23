d77da7fa1e108beff3774bdc46bb08d8ef925fcd
Iagoccarvalho <iago@hctech.com.br>
2022-03-23 09:55:37 -0300

    project created

---
+++ backend/src/test/java/com/icc/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icc.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
